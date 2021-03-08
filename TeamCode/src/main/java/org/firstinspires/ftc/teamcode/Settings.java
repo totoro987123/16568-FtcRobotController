8fb966e8770952fe098c4ae76c08c3894e01446b
lawsonwright <lawsonwright@icloud.com>
2021-03-08 12:41:08 -0500

    Added settings file.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Settings.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public final class Settings {
+
+    public static String FRONT_LEFT = "FL";
+    public static String FRONT_RIGHT = "FR";
+    public static String BACK_LEFT = "BL";
+    public static String BACK_RIGHT = "BR";
+}
