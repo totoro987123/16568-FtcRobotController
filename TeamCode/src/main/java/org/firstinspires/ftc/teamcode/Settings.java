6153e52c63da68c355c244650cba6ad98fc71e6b
lawsonwright <lawsonwright@icloud.com>
2021-03-14 18:50:10 -0400

    Made settings final.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Settings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Settings.java
@@ -2,9 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 public final class Settings {
 
-    public static String FRONT_LEFT = "front_left";
-    public static String FRONT_RIGHT = "front_right";
-    public static String BACK_LEFT = "back_left";
-    public static String BACK_RIGHT = "back_right";
+    public static final String FRONT_LEFT = "front_left";
+    public static final String FRONT_RIGHT = "front_right";
+    public static final String BACK_LEFT = "back_left";
+    public static final String BACK_RIGHT = "back_right";
 
 }
